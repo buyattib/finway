c2f587a2aaef6fa5d721494a4c355f4ce629e39c
buyattib <buyattib29@gmail.com>
2026-01-03 22:19:31 -0300

    new migrations

---
+++ drizzle/0000_create_users_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `users` (
+	`createdAt` text NOT NULL,
+	`updatedAt` text NOT NULL,
+	`id` text(24) PRIMARY KEY NOT NULL,
+	`email` text NOT NULL
+);
+CREATE UNIQUE INDEX `users_email_idx` ON `users` (`email`);
