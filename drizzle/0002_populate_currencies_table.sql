a0c6669579dc4164cd422b1ffce76a00b9e07748
buyattib <buyattib29@gmail.com>
2026-01-03 22:31:10 -0300

    (new) migration: populate currencies table

---
+++ drizzle/0002_populate_currencies_table.sql
@@ -0,0 +1,5 @@
+INSERT INTO "currencies" ("id", "code", "createdAt", "updatedAt") 
+VALUES
+    ('tlknv4n2n4cpgyf3jjxz5c90', 'USD', '2026-01-04T01:20:27.599Z', '2026-01-04T01:20:27.599Z'),
+    ('flo9i92y8p7lnawark6j9vlt', 'ARS', '2026-01-04T01:20:27.599Z', '2026-01-04T01:20:27.599Z'),
+    ('tz85zoxt8pu72yvw6hon82sm', 'EUR', '2026-01-04T01:20:27.599Z', '2026-01-04T01:20:27.599Z');
