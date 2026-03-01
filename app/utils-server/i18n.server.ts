227bfcea1943eb79d271b6cd0ae486213a237e8d
Bautista Buyatti <buyattib29@gmail.com>
2026-03-01 11:46:13 -0300

    small type fix

--- app/utils-server/i18n.server.ts
+++ app/utils-server/i18n.server.ts
@@ -1,6 +1,5 @@
-import type { FlatNamespace, KeyPrefix, Namespace } from 'i18next'
-import { data } from 'react-router'
-import type { RouterContextProvider } from 'react-router'
+import type { FlatNamespace, Namespace } from 'i18next'
+import { data, type RouterContextProvider } from 'react-router'
 
 import { getInstance, getLocale, localeCookie } from '~/middleware/i18next'
 import { LocaleFormSchema } from '~/components/locale-toggle'
@@ -26,15 +25,10 @@ export async function localeAction(formData: FormData) {
 	)
 }
 
-export function getServerT<
-	Ns extends Namespace,
-	TKPrefix extends KeyPrefix<ActualNs> = undefined,
-	ActualNs extends Namespace = Ns extends null ? never : Ns,
->(
+export function getServerT<Ns extends Namespace>(
 	context: Readonly<RouterContextProvider>,
 	ns: Ns & FlatNamespace,
-	keyPrefix?: TKPrefix,
 ) {
 	const locale = getLocale(context)
-	return getInstance(context).getFixedT(locale, ns, keyPrefix)
+	return getInstance(context).getFixedT(locale, ns)
 }
