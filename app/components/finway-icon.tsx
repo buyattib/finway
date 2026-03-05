cfac26f9e9ded3ae5e4910250694b85e6e84773b
Bautista Buyatti <buyattib29@gmail.com>
2026-03-05 13:43:29 -0300

    fix icon reference

--- app/components/finway-icon.tsx
+++ app/components/finway-icon.tsx
@@ -1,9 +1,10 @@
 import { cn } from '~/lib/utils'
+import finwaySvg from '~/assets/finway.svg'
 
 export function FinwayIcon({ size = 'lg' }: { size?: 'md' | 'lg' }) {
 	return (
 		<img
-			src='/app/assets/finway.svg'
+			src={finwaySvg}
 			alt='Finway'
 			className={cn({
 				'w-8 h-8': size === 'md',
