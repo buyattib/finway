7d386da530a5deaaf2512a2a04a0efa58850a562
Bautista Buyatti <buyattib29@gmail.com>
2026-03-14 16:06:16 -0300

    update migration file

--- scripts/migrate.js
+++ scripts/migrate.js
@@ -4,7 +4,12 @@ import { migrate } from 'drizzle-orm/libsql/migrator'
 
 if (!process.env.DB_FILE_NAME) throw new Error('DB_FILE_NAME is required')
 
-const client = createClient({ url: process.env.DB_FILE_NAME })
+const PRODUCTION = process.env.NODE_ENV === 'production'
+
+const client = createClient({
+	url: process.env.DB_FILE_NAME,
+	...(PRODUCTION && { authToken: process.env.TURSO_AUTH_TOKEN }),
+})
 const db = drizzle(client)
 
 console.log('Running migrations...')
